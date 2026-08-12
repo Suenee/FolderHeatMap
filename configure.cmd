02e77eff32d548976765fdd401e70d9057373197
Sueneé <info@easyweby.cz>
2026-08-12 14:23:01 +0200

    Make config launcher work from root or dist

--- configure.cmd
+++ configure.cmd
@@ -1,6 +1,11 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+
+if exist "FolderHeatMapConfig.exe" (
+    start "" "FolderHeatMapConfig.exe"
+    exit /b 0
+)
 if exist "dist\FolderHeatMapConfig.exe" (
     start "" "dist\FolderHeatMapConfig.exe"
     exit /b 0
@@ -9,6 +14,7 @@ if exist "build\Release\FolderHeatMapConfig.exe" (
     start "" "build\Release\FolderHeatMapConfig.exe"
     exit /b 0
 )
+
 echo FolderHeatMapConfig.exe was not found.
 echo Run upgrade.cmd first.
 pause
