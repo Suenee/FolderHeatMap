5ee19c7f9c0048ec7a2dc5120ab5df7114f9bf9a
Sueneé <info@easyweby.cz>
2026-08-12 14:16:00 +0200

    Add one-click FolderHeatMap configuration launcher

---
+++ configure.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+if exist "dist\FolderHeatMapConfig.exe" (
+    start "" "dist\FolderHeatMapConfig.exe"
+    exit /b 0
+)
+if exist "build\Release\FolderHeatMapConfig.exe" (
+    start "" "build\Release\FolderHeatMapConfig.exe"
+    exit /b 0
+)
+echo FolderHeatMapConfig.exe was not found.
+echo Run upgrade.cmd first.
+pause
+exit /b 1
