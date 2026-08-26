57ab4dee4361d215b31af0eb2bc62e6d02f57d4b
Sueneé <info@easyweby.cz>
2026-08-27 01:18:58 +0200

    Bump upgrade launcher revision to 1.27

--- upgrade.cmd
+++ upgrade.cmd
@@ -2,7 +2,7 @@
 cls
 setlocal EnableExtensions EnableDelayedExpansion
 
-set "UPGRADE_REV=1.26-runtime-version-fix"
+set "UPGRADE_REV=1.27-tc-deploy-guard"
 set "REPO_DIR=%~dp0"
 if "!REPO_DIR:~-1!"=="\" set "REPO_DIR=!REPO_DIR:~0,-1!"
 cd /d "!REPO_DIR!"
