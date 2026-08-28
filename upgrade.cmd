4ccddacef04c46e0ed57411617c46c01d47bdd93
Sueneé <info@easyweby.cz>
2026-08-28 23:39:17 +0200

    Bump upgrade launcher to 1.44

--- upgrade.cmd
+++ upgrade.cmd
@@ -2,7 +2,7 @@
 cls
 setlocal EnableExtensions EnableDelayedExpansion
 
-set "UPGRADE_REV=1.43-rapid-move-returned-object"
+set "UPGRADE_REV=1.44-destructive-lifecycle-trace"
 set "RUN_TEST=0"
 if /I "%~1"=="--test" (
     set "RUN_TEST=1"
