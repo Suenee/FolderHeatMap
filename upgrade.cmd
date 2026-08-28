b4a1a9fd5d760e614e392514ff6e1d7824585b39
Sueneé <info@easyweby.cz>
2026-08-29 00:45:15 +0200

    Update upgrade launcher to 1.46

--- upgrade.cmd
+++ upgrade.cmd
@@ -2,7 +2,7 @@
 cls
 setlocal EnableExtensions EnableDelayedExpansion
 
-set "UPGRADE_REV=1.45-diagnostic-version-summary"
+set "UPGRADE_REV=1.46-queued-file-id-rapid-move"
 set "RUN_TEST=0"
 if /I "%~1"=="--test" (
     set "RUN_TEST=1"
